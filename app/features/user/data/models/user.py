71869d83f1fd77d9f6e68fe49b1a59978597338b
amway <angelmanuelwayar@gmail.com>
2023-05-31 10:05:47 -0400

    rename files and create base_unit_of_work.py, base_repository.py

--- app/features/user/data/models/user.py
+++ app/features/user/data/models/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import Mapped
 
-from app.core.models.models import Base
+from app.core.models.base_model import Base
 
 
 class User(Base):
