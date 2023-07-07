df7c81e26aae1f31d689d8bc07d4233ae8f651f7
amway <angelmanuelwayar@gmail.com>
2023-07-07 18:36:18 -0400

    update club_routes.py

--- app/features/club/api/routes/club_routes.py
+++ app/features/club/api/routes/club_routes.py
@@ -1,5 +1,6 @@
 from app.features.club.api.routes.get_club_route import get_club
 from app.features.club.api.routes.get_clubs_route import get_clubs
+from app.features.club.api.routes.update_club_route import update_club
 from app.features.club.api.routes.create_club_route import create_club, router
 
 club_router = router
